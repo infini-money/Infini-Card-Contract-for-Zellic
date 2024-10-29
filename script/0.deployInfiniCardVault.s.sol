943e9bcf3b5c957dfea2fd36d2d7276ac0bbbf98
shaneson <shaneson@shanesondeMacBook-Pro.local>
2024-10-29 09:44:29 +0800

    prepare deploy

--- script/0.deployInfiniCardVault.s.sol
+++ script/0.deployInfiniCardVault.s.sol
@@ -14,17 +14,16 @@ contract DeployInfiniCardVault is Script {
     function run() external {
         uint256 adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
         address adminRole = vm.addr(adminPrivateKey);
-        
+
         // address USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
         address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         address USDE = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
-        address shaneson = 0x790ac11183ddE23163b307E3F7440F2460526957;
         
         vm.startBroadcast(adminPrivateKey);
 
         InfiniCardVault vault = new InfiniCardVault(adminRole, adminRole, adminRole);
         vault.grantRole(vault.DEFAULT_ADMIN_ROLE(), adminRole);
-        vault.addCustodianToWhiteList(shaneson);
+        // vault.addCustodianToWhiteList(shaneson);
 
         // strategys
         address EthenaMintingAddress = 0xe3490297a08d6fC8Da46Edb7B6142E4F461b62D3;
--- test/morpho/morphoTest.t.sol
+++ test/morpho/morphoTest.t.sol
@@ -9,7 +9,6 @@ import "forge-std/console.sol";
 import {stdStorage, StdStorage, Test} from "forge-std/Test.sol";
 import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-
 contract MorphoTesting is Test {
     address market = 0xd63070114470f685b75B74D60EEc7c1113d33a3D;
     address christian = 0x0DB87155511f190034a2b73D98F699bFeBFbD85F;
